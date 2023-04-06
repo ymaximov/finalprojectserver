f6525ab3f563c8227095cb566f0456e201d6849b
Yaniv Maximov <yanivmaximov@icloud.com>
2023-04-06 11:16:26 +0300

    final

--- server.js
+++ server.js
@@ -7,12 +7,9 @@ const userRoute = require('./routes/userRoute')
 const adminRoute = require('./routes/adminRoute')
 const doctorRoute = require('./routes/doctorsRoute')
 const cors = require('cors')
-var corsOptions = {
-    origin: 'https://642be044a7147905cfaef0ca--dainty-mooncake-91b478.netlify.app',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
-  app.use(cors(corsOptions));
-  app.use(express.json());
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(port, ()=> console.log(`Node Server Listening on Port ${port}`))
 
